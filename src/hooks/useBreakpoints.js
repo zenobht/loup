9f43f3a41cc3806ba662f0b7e299ed5821bbf584
bharat joshi <bharatjoshi.p@gmail.com>
2019-10-27 23:37:59 +1100

    add hook to change image based on resolution

---
+++ src/hooks/useBreakpoints.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+
+const useBreakpoints = () => {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth)
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  })
+
+  return width
+}
+
+export default useBreakpoints
