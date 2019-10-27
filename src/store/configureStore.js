d708cf93c3b8264c4796aa246ab6c173cfd9c715
bharat joshi <bharatjoshi.p@gmail.com>
2019-10-28 06:34:45 +1100

    add redux devtool support

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import reducer from '../reducers/reducer'
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 export default store
